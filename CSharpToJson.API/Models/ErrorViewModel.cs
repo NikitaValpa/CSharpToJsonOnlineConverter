c659f885148126f62e78965c657a0155cce76b78
Nikita Valpa <nikich.valpa@yandex.ru>
2023-04-16 23:54:49 +0500

    Add project files.

---
+++ CSharpToJson.API/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSharpToJson.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
