4b004d1d32685b9cbcdc63188d1624ad7603b498
Nikita Valpa <nikich.valpa@yandex.ru>
2024-02-23 23:33:48 +0500

    added monaco editor to main page and copy to clipboard function

--- CSharpToJson.WebAsm/Client/Pages/Index.razor.js
+++ CSharpToJson.WebAsm/Client/Pages/Index.razor.js
@@ -1,40 +1,3 @@
-export function update(text) {
-    const resultElement = document.querySelector("#highlighting-content");
-
-    if (text[text.length - 1] === "\n") { // If the last character is a newline character
-        text += " "; // Add a placeholder space character to the final line 
-    }
-
-    resultElement.innerHTML = text.replace(new RegExp("&", "g"), "&amp;").replace(new RegExp("<", "g"), "&lt;");
-    Prism.highlightElement(resultElement);
-}
-
-export function syncScroll(element) {
-    const resultElement = document.querySelector("#highlighting");
-    resultElement.scrollTop = element.scrollTop;
-    resultElement.scrollLeft = element.scrollLeft;
-}
-
-export function observe() {
-    const resizeObserver = new window.ResizeObserver(elements => {
-        const codeBlock = document.querySelector('#codeBlock');
-
-        for (const elem of elements) {
-            if (elem && codeBlock) {
-                codeBlock.style.height = `${elem.borderBoxSize[0].blockSize}px`;
-            }
-        }
-    });
-
-    resizeObserver.observe(document.querySelector('#floatingTextarea'));
-    initializeHighlight();
-
-    function initializeHighlight() {
-        Prism.highlightElement(document.querySelector("#example-csharp"));
-        Prism.highlightElement(document.querySelector("#example-json"));
-    }
-}
-
 export function stringify(json) {
     try {
         const validJson = JSON.parse(json);
@@ -49,4 +12,11 @@ export function highlightJson(json) {
     const jsonOut = document.querySelector("#jsonOutput");
     jsonOut.innerHTML = json;
     Prism.highlightElement(jsonOut);
+}
+
+export function copyToClipboard(jsonBlockElement) {
+    if (jsonBlockElement.innerText) {
+        navigator.clipboard.writeText(jsonBlockElement.innerText);
+        console.log('copied');
+    }
 }
